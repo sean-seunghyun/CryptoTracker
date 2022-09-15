43e1bd1a3edb05307127da6e97d07a53bfaf175b
sean <flyseankim93@gmail.com>
2022-09-15 15:02:20 +0900

    Clean code with NetworkingManager, error handling with Enum

--- CryptoTracker/Services/CoinDataService.swift
+++ CryptoTracker/Services/CoinDataService.swift
@@ -22,36 +22,19 @@ class CoinDataService{
     private func getCoins(){
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .background))
-            .tryMap(handleOutput)
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(for: url)
             .decode(type: [Coin].self, decoder: JSONDecoder())
-            .sink { completeion in
-                switch completeion{
-                case .finished:
-                     break
-                case .failure(let error) :
-                    print("error downloading data: \(error)")
-                }
-            } receiveValue: { [weak self] returnedCoins in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedCoins in
                 guard let self = self else { return }
                 
                 self.allCoins = returnedCoins
                 // API call을 여러번 하는 경우 여기서 cancel을 해준다.
                 // 여기서는 굳이 해줄 필요가 없음.
                 self.coinSubscription?.cancel()
-            }
+            })
+        
 
     }
     
-    private func handleOutput(output: URLSession.DataTaskPublisher.Output) throws -> Data{
-        guard
-            let response = output.response as? HTTPURLResponse,
-            response.statusCode >= 200 && response.statusCode < 300 else {
-            throw URLError(.badServerResponse)
-        }
-        return output.data
-    }
     
 }
---
+++ CryptoTracker/Utilities/NetworkingManager.swift
@@ -0,0 +1,55 @@
+//
+//  NetworkingManager.swift
+//  CryptoTracker
+//
+//  Created by sean on 2022/09/15.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager{
+    
+    enum NetworkingError: LocalizedError{
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self{
+            case .badURLResponse(url: let url):
+                return "[😡] bad response from URL: \(url)"
+            case .unknown :
+                return "[😰] unknown error"
+            }
+        }
+    }
+    
+    static func download(for url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .tryMap({try handleOutput(output: $0, url: url)})
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher() // return to AnyPublisher<Data, Error>
+    }
+    
+    static func handleOutput(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data{
+        guard
+            let response = output.response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+            
+            // 여기서 error가 나면 handleCompletion에서 error을 받아서 출력함
+        }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>){
+        
+        switch completion{
+        case .finished:
+             break
+        case .failure(let error) :
+            print("error downloading data: \(error.localizedDescription)")
+        }
+    }
+}
